7383ceb64f3e93dc17a9f9783b7bb33f131d3551
Jo <tkwei9@163.com>
2021-10-12 20:41:54 +0800

    新增 访问8080端口，显示hello world

--- src/main/java/com/jokwei/gazelle/GazelleApplication.java
+++ src/main/java/com/jokwei/gazelle/GazelleApplication.java
@@ -2,9 +2,17 @@ package com.jokwei.gazelle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GazelleApplication {
+
+    @RequestMapping("/")
+    String index() {
+        return "hello world";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GazelleApplication.class, args);
